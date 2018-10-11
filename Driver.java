6b1f59d5a56d2e54b7da15115ab6bdf01d3e2010
James-Chun <jchun00@stuy.edu>
2018-10-10 21:27:31 -0400

    some progress

--- Driver.java
+++ Driver.java
@@ -2,6 +2,8 @@ public class Driver {
     public static void main(String[] args) {
         Point P1 = new Point (1.0,2.0);
         Point P2 = new Point (3.0,4.0);
-        System.out.println (Point.distanceTo(P1,P2));
+        Point P3 = new Point (1.0,5.0);
+        System.out.println ( P1.distanceTo(P2) );
+        System.out.println ( P1.distanceTo(P3) );
     }
 }
--- Point.java
+++ Point.java
@@ -1,18 +1,27 @@
 public class Point {
-    private double[] point = new double[2];
+    private double x;
+    private double y;
     
-    public Point (double x,double y) {
-        point[0] = x;
-        point[1] = y;
+    public Point (double xCoord,double yCoord) {
+        x = xCoord;
+        y = yCoord;
     }
+    public Point (Point point) {
+        x = point.x;
+        y = point.y;
+    }
+    
     
     public double getX() {
-        return point[0];
+        return this.x;
     }
     public double getY() {
-        return point[1];
+        return this.y;
     }
-    public double distanceTo(double[] point1, double[] point2) {
-        return Math.sqrt( Math.pow(point1.getX() - point2.getX(),2) + Math.pow(point1.getY() - point2.getY(), 2) );
+    public double distanceTo(Point p2) {
+        return Math.sqrt( Math.pow(this.x - p2.x,2) + Math.pow(this.y - p2.y, 2) );
+    }
+    public double distance(Point p1,Point p2) {
+        return Math.sqrt( Math.pow(p1.x - p2.x,2) + Math.pow(p2.y - p2.y, 2) );
     }
 }
